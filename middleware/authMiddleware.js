893988e99b67ab55a3821729a1545cc48dab3097
Kanesha K <kaneshak79@gmail.com>
2026-07-02 09:40:56 +0530

    Cleanup: Remove unused auth middleware code
    Removed commented-out code for JWT authentication middleware.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,20 +1,3 @@
-// import jwt from "jsonwebtoken";
-// import User from "../models/User.js";
-
-// const protect = async (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization?.split(" ")[1];
-
-//     if (!token) return res.status(401).json({ msg: "No token" });
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = await User.findById(decoded.id);
-
-//     next();
-//   } catch {
-//     res.status(401).json({ msg: "Invalid token" });
-//   }
-// };
 
 // export default protect;
 
